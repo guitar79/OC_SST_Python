695af877c88849b7199fa176f179db03db8648f9
Kiehyun Kevin Park <guitar79@naver.com>
2020-05-29 15:18:08 +0900

    Add files via upload

--- kosc_crawler_Multithreading.py
+++ kosc_crawler_Multithreading.py
@@ -20,15 +20,19 @@ class crawler():
         
     def fetch(self, moids_data_el):
         base_url = 'http://222.236.46.45/nfsdb/'
-        save_dir_name = '../{0}{1}/'.format(moids_data_el[0][:-1], moids_data_el[1][:-1])
+        save_dir_name = '../{0}_{1}_{2}/'.format(moids_data_el[1][:-1], moids_data_el[4], moids_data_el[5])
         print(save_dir_name)
         if not os.path.exists(save_dir_name):
             os.makedirs(save_dir_name)
             print ('*'*80)
             print ('{0} is created.'.format(save_dir_name))
 		
-        filename = '{0}.{1:04d}.{2:02d}{3:02d}.{4:02d}{5:02d}.{6}.hdf.zip'\
+        if moids_data_el[5] == 'NOAA' :
+            filename = '{0}{1:04d}.{2:02d}{3:02d}.{4:02d}{5:02d}.{6}'\
                     .format(moids_data_el[2], self.year, self.month, self.day, self.hour, self.minute, moids_data_el[3])
+        else :                     
+            filename = '{0}.{1:04d}.{2:02d}{3:02d}.{4:02d}{5:02d}.{6}'\
+                .format(moids_data_el[2], self.year, self.month, self.day, self.hour, self.minute, moids_data_el[3])
         
         url = '{0}{1}{2:04d}/{3:02d}/{4:02d}/{5}{6}'.\
                     format(base_url, moids_data_el[0], 
@@ -58,8 +62,7 @@ class crawler():
                             
             except Exception as err : 
                 print('X'*60)
-                print('{0}, {1}\n'.format(err, url))
-                sys.stderr.write("Thread #{0:d} failed...retry\n".format(self.threadno))
+                sys.stderr.write("Thread #{0:d} failed...\n{1}, {2}\n".format(self.threadno, err, url))
                 pass
                   
 #crawler for single day
@@ -68,7 +71,6 @@ class crawler_month(threading.Thread):
         threading.Thread.__init__(self)
         self.year = year
         self.month = month
-        
         self.threadno = threadno
         sys.stderr.write('Thread #{0:d} started...\n'.format(self.threadno))
 
@@ -83,17 +85,21 @@ class crawler_month(threading.Thread):
                         
 #</nfsdb/MODIST/2016/05/08/L2/MODOCT.2016.0508.1436.terra-1.hdf.zip>
 #</nfsdb/MODIST/2016/05/09/L2/MODOCBOX.2016.0509.0236.terra-1.hdf.zip>
+#</nfsdb/NOAA/2013/12/22/L2/2013.1222.0659.noaa-18.sst.asc.zip>
 
 threadno = 0
-moids_data_els = [('MODISA/', 'L2/', 'MYDOCT', 'aqua-1'),                  
-                  ('MODIST/', 'L2/', 'MODOCT', 'terra-1'),
-                  ('MODISA/', 'L2/', 'MYDOCX', 'aqua-1'),
-                  ('MODIST/', 'L2/', 'MODOCX', 'terra-1'),
-                  ('MODISA/', 'L2/', 'MYDOCBOX', 'aqua-1'),
-                  ('MODIST/', 'L2/', 'MODOCBOX', 'terra-1'),
-                  ('MODISA/', 'L2/', 'MYDOCSST', 'aqua-1'), # after 2019
-                  ('MODIST/', 'L2/', 'MODOCSST', 'terra-1') # after 2019
-                  ]
+moids_data_els = [('MODISA/', 'L2/', 'MYDOCT', 'aqua-1.hdf.zip', 'SST', 'MODIS'),                  
+                  ('MODIST/', 'L2/', 'MODOCT', 'terra-1.hdf.zip', 'SST', 'MODIS'),
+                  ('MODISA/', 'L2/', 'MYDOCX', 'aqua-1.hdf.zip', 'CHL', 'MODIS'),
+                  ('MODIST/', 'L2/', 'MODOCX', 'terra-1.hdf.zip', 'CHL', 'MODIS'),
+                  ('MODISA/', 'L2/', 'MYDOCBOX', 'aqua-1.hdf.zip', 'CHL', 'MODIS'),
+                  ('MODIST/', 'L2/', 'MODOCBOX', 'terra-1.hdf.zip', 'CHL', 'MODIS'),
+                  ('MODISA/', 'L2/', 'MYDOCSST', 'aqua-1.hdf.zip', 'SST', 'MODIS'), # after 2019
+                  ('MODIST/', 'L2/', 'MODOCSST', 'terra-1.hdf.zip', 'SST', 'MODIS'),# after 2019
+                  ('NOAA/', 'L2/', '', 'noaa-16.sst.asc.zip', 'SST', 'NOAA'),
+                  ('NOAA/', 'L2/', '', 'noaa-17.sst.asc.zip', 'SST', 'NOAA'),
+                  ('NOAA/', 'L2/', '', 'noaa-18.sst.asc.zip', 'SST', 'NOAA'),
+                  ('NOAA/', 'L2/', '', 'noaa-19.sst.asc.zip', 'SST', 'NOAA')]
 
 for Yr in range(2011,2020):
     for Mo in range(1,13):
